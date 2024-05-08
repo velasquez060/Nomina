90847037df0671cee307d64cff5d84e409557f0f
Nolberto Velasquez <nolbertovargad115@gmail.com>
2024-05-08 07:31:31 -0500

    Ajustes error al momento de agregar un empleado
    Ajustes error al momento de agregar un empleado

--- Nomina/Conexion/conexion.php
+++ Nomina/Conexion/conexion.php
@@ -5,9 +5,9 @@
   $password = "12345";
 
   try{
-    $conexion = new PDO("mysql:host=$server;dbname:$database;",$user,$password);
+    $conexion = new PDO("mysql:host=$server;dbname=$database;",$user,$password);
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexión exitosa a la base de datos";
+    //echo "Conexión exitosa a la base de datos";
 
   }catch(PDOException $e){
     echo "Error al hacer la conexion con la base de datos" . $e->getMessage();
