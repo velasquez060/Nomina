da14da8086b63b5c874397eefe1fb8964e4bcfa9
Nolberto Velasquez <nolbertovargad115@gmail.com>
2024-05-11 15:51:22 -0500

    Ajustes Validacion de formulario Agregar Empleado
    Ajustes Validacion de formulario Agregar Empleado

--- Nomina/Principal/Empleado/AgregarEmpleado.php
+++ Nomina/Principal/Empleado/AgregarEmpleado.php
@@ -1,12 +1,9 @@
 <?php
 
-require('../../conexion/conexion.php'); // importar archivo de conexion
+require('../../conexion/conexion.php');
 
-
-
-
-if (isset($_POST['registrar'])) {
-  $nombre = htmlspecialchars($_POST['textNombre']); //capatura de datos
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $nombre = htmlspecialchars($_POST['textNombre']); 
   $apellido = htmlspecialchars($_POST['textApellido']);
   $cedula = htmlspecialchars($_POST['textCedula']);
   $fechaNacimiento = htmlspecialchars($_POST['textFechaNacimiento']);
@@ -24,50 +21,63 @@ if (isset($_POST['registrar'])) {
   $fechaTerminacion = htmlspecialchars($_POST['textFechaTerminacion']);
   $contactoEmergencia = htmlspecialchars($_POST['textContacto']);
   $numeroContactoEmergencia = htmlspecialchars($_POST['textNumeroContacto']);
+
+  if (
+    !empty($nombre) && !empty($apellido) && !empty($cedula) && !empty($fechaNacimiento) &&
+    !empty($celular) && !empty($direccion) && !empty($correo) && !empty($estadoCivil) && !empty($arl) && !empty($fondoPensiones) && !empty($fondoCesantias) && !empty($entidadBancaria) && !empty($numeroCuenta) && !empty($fechaIngreso) && !empty($contactoEmergencia) && !empty($numeroContactoEmergencia)
+) {
+
+  try {
+
+    $sql = "INSERT INTO empleado (nombre, apellido, cedula, fechaNacimiento, celular, direccion, correo, estadoCivil, eps, arl, fondoPensiones, fondoCesantias, entidadBancaria, numeroCuenta, fechaIngreso, fechaTerminacion, contactoEmergencia, numeroContactoEmergencia) VALUES (:nombre, :apellido, :cedula, :fechaNacimiento, :celular, :direccion, :correo, :estadoCivil, :eps, :arl, :fondoPensiones, :fondoCesantias, :entidadBancaria, :numeroCuenta, :fechaIngreso, :fechaTerminacion, :contactoEmergencia, :numeroContactoEmergencia)";
+  
+  $stmt = $conexion->prepare($sql);
+  
+  $stmt->bindParam(':nombre', $nombre);
+  $stmt->bindParam(':apellido', $apellido);
+  $stmt->bindParam(':cedula', $cedula);
+  $stmt->bindParam(':fechaNacimiento', $fechaNacimiento);
+  $stmt->bindParam(':celular', $celular);
+  $stmt->bindParam(':direccion', $direccion);
+  $stmt->bindParam(':correo', $correo);
+  $stmt->bindParam(':estadoCivil', $estadoCivil);
+  $stmt->bindParam(':eps', $eps);
+  $stmt->bindParam(':arl', $arl);
+  $stmt->bindParam(':fondoPensiones', $fondoPensiones);
+  $stmt->bindParam(':fondoCesantias', $fondoCesantias);
+  $stmt->bindParam(':entidadBancaria', $entidadBancaria);
+  $stmt->bindParam(':numeroCuenta', $numeroCuenta);
+  $stmt->bindParam(':fechaIngreso', $fechaIngreso);
+  $stmt->bindParam(':fechaTerminacion', $fechaTerminacion);
+  $stmt->bindParam(':contactoEmergencia', $contactoEmergencia);
+  $stmt->bindParam(':numeroContactoEmergencia', $numeroContactoEmergencia);
+  
+  
+  
+      $stmt->execute();
+  
+  
+      if ($stmt->rowCount() > 0) {
+        //echo "<script>alert('Usuario Agregado Correctamente');</script>";
+      
+        header("Location: ListaEmpleados.php");
+        //exit();
+  
+        
+      }
+    
+  } catch (Exception $e) {
+    echo $e->getMessage();
+  }
+    
+} else {
+  echo "<script>alert('por favor rellene todos los campos');</script>";
+}
 } else {
   //echo "No se recibieron datos del formulario.";
 
 }
 
-
-try {
-
-  $sql = "INSERT INTO empleado (nombre, apellido, cedula, fechaNacimiento, celular, direccion, correo, estadoCivil, eps, arl, fondoPensiones, fondoCesantias, entidadBancaria, numeroCuenta, fechaIngreso, fechaTerminacion, contactoEmergencia, numeroContactoEmergencia) VALUES (:nombre, :apellido, :cedula, :fechaNacimiento, :celular, :direccion, :correo, :estadoCivil, :eps, :arl, :fondoPensiones, :fondoCesantias, :entidadBancaria, :numeroCuenta, :fechaIngreso, :fechaTerminacion, :contactoEmergencia, :numeroContactoEmergencia)";
-
-$stmt = $conexion->prepare($sql);
-
-$stmt->bindParam(':nombre', $nombre);
-$stmt->bindParam(':apellido', $apellido);
-$stmt->bindParam(':cedula', $cedula);
-$stmt->bindParam(':fechaNacimiento', $fechaNacimiento);
-$stmt->bindParam(':celular', $celular);
-$stmt->bindParam(':direccion', $direccion);
-$stmt->bindParam(':correo', $correo);
-$stmt->bindParam(':estadoCivil', $estadoCivil);
-$stmt->bindParam(':eps', $eps);
-$stmt->bindParam(':arl', $arl);
-$stmt->bindParam(':fondoPensiones', $fondoPensiones);
-$stmt->bindParam(':fondoCesantias', $fondoCesantias);
-$stmt->bindParam(':entidadBancaria', $entidadBancaria);
-$stmt->bindParam(':numeroCuenta', $numeroCuenta);
-$stmt->bindParam(':fechaIngreso', $fechaIngreso);
-$stmt->bindParam(':fechaTerminacion', $fechaTerminacion);
-$stmt->bindParam(':contactoEmergencia', $contactoEmergencia);
-$stmt->bindParam(':numeroContactoEmergencia', $numeroContactoEmergencia);
-
-
-
-    $stmt->execute();
-
-
-    if ($stmt->rowCount() > 0) {
-      echo "<script>alert('Usuario Agregado Correctamente');</script>";
-      //exit();
-    }
-  
-} catch (Exception $e) {
-  echo $e->getMessage();
-}
 ?>
 
 <!DOCTYPE html>
@@ -79,6 +89,9 @@ $stmt->bindParam(':numeroContactoEmergencia', $numeroContactoEmergencia);
   <link rel="stylesheet" href="../../Css/empleado.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+  <script src="../../Js/prueba.js"></script>
   <title>Document</title>
 </head>
 
@@ -87,7 +100,7 @@ $stmt->bindParam(':numeroContactoEmergencia', $numeroContactoEmergencia);
   <H1 class="tituloempleado">Ficha Empleado</H1>
 
   <div class="container d-flex justify-content-center">
-    <form class="row g-3 " action="conexionEmpleado.php" method="POST">
+    <form class="row g-3 " action="AgregarEmpleado.php" method="POST">
       <div class="col-md-6">
         <label class="form-label">Nombre:</label>
         <input type="texto" name="textNombre" class="form-control" id="inputNombre">
@@ -163,12 +176,13 @@ $stmt->bindParam(':numeroContactoEmergencia', $numeroContactoEmergencia);
 
 
       <div class="col-12">
-        <input type="submit" value="Registrar" style="background-color: #3176E0;" name="registrar">
+        <input type="submit" value="Registrar" style="background-color: #3176E0;" name="registrar" id="prueba" onclick="prueba">
       </div>
     </form>
   </div>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
 
 </html>
