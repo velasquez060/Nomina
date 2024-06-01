61644bfdbc2eab04ee2bb54016747d384aea17c0
Nolberto Velasquez <nolbertovargad115@gmail.com>
2024-06-01 18:58:04 -0500

    Ajuste de rutas y se sube el problema de boostrap no lo esta tomando
    Ajuste de rutas y se sube el problema de boostrap no lo esta tomando

--- Nomina/Empleado/AgregarEmpleado.php
+++ Nomina/Empleado/AgregarEmpleado.php
@@ -1,7 +1,7 @@
 <?php
 
-require('../../conexion/conexion.php');
-//include('Menu.php');
+require('../conexion/conexion.php');
+include('../Menu.php');
 
 
 
@@ -99,10 +99,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../../Css/empleado.css">
+  <link rel="stylesheet" href="../Css/empleado.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="../../Css/menu.css">
+  <link rel="stylesheet" href="../Css/menu.css">
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
   <title>Document</title>
 
@@ -219,9 +219,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   </div>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <script src="../../js/menu.js"></script>
-  <script src="../../js/validacionCampos.js"></script>
+  <script src="../js/menu.js"></script>
+  <script src="../js/validacionCampos.js"></script>
 
   
 </body>
