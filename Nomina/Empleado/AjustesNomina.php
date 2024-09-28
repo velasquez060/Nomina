c50f44b05ba29000d6681348d7842a0915aa51b5
Nolberto Velasquez <nolbertovargad115@gmail.com>
2024-09-28 16:40:02 -0500

    fix: nueva funcionalidad de traer el registro de configuración nomina
    se agrega funcionalidad al momento de agregar un registro de la configuración de los datos que se repiten en cada uno de los usuarios , además consultar si ya existe un registro que se puede visualizar en el formulario

--- Nomina/Empleado/AjustesNomina.php
+++ Nomina/Empleado/AjustesNomina.php
@@ -3,55 +3,104 @@ include("../Menu.php");
 require('../conexion/conexion.php');
 $objconexion = new conexion();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $salariobasico = htmlspecialchars($_POST["salarioBasico"]);
-  $valorhora = htmlspecialchars($_POST["valorHora"]);
-  $valorHoraExtraDiurna = htmlspecialchars($_POST["valorHoraExtraDiurna"]);
-  $valorHoraExtraNocturna = htmlspecialchars($_POST["valorHoraExtraNocturna"]);
-  $valorHoraExtraDominical = htmlspecialchars($_POST["valorHoraExtraDominical"]);
-  $valorHoraExtraDominicalNocturna = htmlspecialchars($_POST["valorHoraExtraDominicalNocturna"]);
-  $valorHoraDomingosFestivos = htmlspecialchars($_POST["valorHoraDomingosFestivos"]);
-  $valorRecargoNocturno = htmlspecialchars($_POST["valorRecargoNocturno"]);
-  $valorAuxilioTransporte = htmlspecialchars($_POST["valorAuxilioTransporte"]);
-  $valorSalud = htmlspecialchars($_POST["valorSalud"]);
-  $valorPension = htmlspecialchars($_POST["valorPension"]);
-  $ajustePorcentual = htmlspecialchars($_POST["ajustePorcentual"]) ? $_POST['ajustePorcentual'] : null;
+
+$sql2= "select * from configuracion";
+$stmt_verificar = $objconexion->prepare($sql2);
+$stmt_verificar->execute();
+$Consultatabla = $stmt_verificar->rowCount();
+
+if ($Consultatabla >= 1) {
+  $salariobasico = "";
+  $valorhora = "";
+  $valorHoraExtraDiurna = "";
+  $valorHoraExtraNocturna = "";
+  $valorHoraExtraDominical = "";
+  $valorHoraExtraDominicalNocturna = "";
+  $valorHoraDomingosFestivos = "";
+  $valorRecargoNocturno = "";
+  $valorAuxilioTransporte = "";
+  $valorSalud = "";
+  $valorPension = "";
+  $ajustePorcentual = "";
+  $row_verificar = $stmt_verificar->fetch(PDO::FETCH_ASSOC);
 
 
-  if (!empty($salariobasico) && !empty($valorhora) && !empty($valorHoraExtraDiurna) && !empty($valorHoraExtraNocturna) && !empty($$valorHoraExtraDominical) && !empty($valorHoraExtraDominicalNocturna) && !empty($valorHoraDomingosFestivos) && !empty($valorRecargoNocturno) && !empty($valorAuxilioTransporte) && !empty($valorSalud) && !empty($valorPension)) {
 
-    try {
-      $sql = "INSERT INTO configuracion (salario_basico, valor_hora, valor_hora_extra_diurna, valor_hora_extra_nocturna, valor_hora_extra_dominical, valor_hora_extra_dominical_nocturna, valor_hora_domingos_festivos, valor_recargo_nocturno, valor_auxilio_transporte, valor_salud, valor_pension, ajuste_porcentual) VALUES (:salariobasico, :valorhora, :valorHoraExtraDiurna,:valorHoraExtraNocturna, :valorHoraExtraDominical, :valorHoraExtraDominicalNocturna, :valorHoraDomingosFestivos, :valorRecargoNocturno, :valorAuxilioTransporte, :valorSalud, :valorPension, :ajustePorcentual)";
+  $salariobasico = isset($row_verificar['salario_basico']) ? $row_verificar['salario_basico'] : "";
+  $valor_hora = isset($row_verificar['valor_hora']) ? $row_verificar['valor_hora'] : "";
+  $valor_hora_extra_diurna = isset($row_verificar['valor_hora_extra_diurna']) ? $row_verificar['valor_hora_extra_diurna'] : "";
+  $valor_hora_extra_nocturna = isset($row_verificar['valor_hora_extra_nocturna']) ? $row_verificar['valor_hora_extra_nocturna'] : "";
+  $valor_hora_extra_dominical = isset($row_verificar['valor_hora_extra_dominical']) ? $row_verificar['valor_hora_extra_dominical'] : "";
+  $valor_hora_extra_dominical_nocturna = isset($row_verificar['valor_hora_extra_dominical_nocturna']) ? $row_verificar['valor_hora_extra_dominical_nocturna'] : "";
+  $valor_hora_domingos_festivos = isset($row_verificar['valor_hora_domingos_festivos']) ? $row_verificar['valor_hora_domingos_festivos'] : "";
+  $valor_recargo_nocturno = isset($row_verificar['valor_recargo_nocturno']) ? $row_verificar['valor_recargo_nocturno'] : "";
+  $valor_auxilio_transporte = isset($row_verificar['valor_auxilio_transporte']) ? $row_verificar['valor_auxilio_transporte'] : "";
+  $valor_auxilio_transporte = isset($row_verificar['valor_auxilio_transporte']) ? $row_verificar['valor_auxilio_transporte'] : "";
+  $valor_salud = isset($row_verificar['valor_salud']) ? $row_verificar['valor_salud'] : "";
+  $valor_pension = isset($row_verificar['valor_pension']) ? $row_verificar['valor_pension'] : "";
+  $ajuste_porcentual = isset($row_verificar['ajuste_porcentual']) ? $row_verificar['ajuste_porcentual'] : "";
 
-      $stmt = $objconexion->prepare($sql);
-      $stmt->bindParam(':salariobasico', $salariobasico);
-      $stmt->bindParam(':valorhora', $valorhora);
-      $stmt->bindParam(':valorHoraExtraDiurna', $valorHoraExtraDiurna);
-      $stmt->bindParam(':valorHoraExtraNocturna', $valorHoraExtraNocturna);
-      $stmt->bindParam(':valorHoraExtraDominical', $valorHoraExtraDominical);
-      $stmt->bindParam(':valorHoraExtraDominicalNocturna', $valorHoraExtraDominicalNocturna);
-      $stmt->bindParam(':valorHoraDomingosFestivos', $valorHoraDomingosFestivos);
-      $stmt->bindParam(':valorRecargoNocturno', $valorRecargoNocturno);
-      $stmt->bindParam(':valorAuxilioTransporte', $valorAuxilioTransporte);
-      $stmt->bindParam(':valorSalud', $valorSalud);
-      $stmt->bindParam(':valorPension', $valorPension);
-      $stmt->bindParam(':ajustePorcentual', $ajustePorcentual);
 
-      if ($stmt->execute()) {
-        echo "<script>alert('Configuración Agregada Correctamente');</script>";
-        //header("Location: ListaEmpleados.php");
-      } else {
-        echo "Error al agregar Configuración.";
+
+  
+}else{
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $salariobasico = htmlspecialchars($_POST["salarioBasico"]);
+    $valorhora = htmlspecialchars($_POST["valorHora"]);
+    $valorHoraExtraDiurna = htmlspecialchars($_POST["valorHoraExtraDiurna"]);
+    $valorHoraExtraNocturna = htmlspecialchars($_POST["valorHoraExtraNocturna"]);
+    $valorHoraExtraDominical = htmlspecialchars($_POST["valorHoraExtraDominical"]);
+    $valorHoraExtraDominicalNocturna = htmlspecialchars($_POST["valorHoraExtraDominicalNocturna"]);
+    $valorHoraDomingosFestivos = htmlspecialchars($_POST["valorHoraDomingosFestivos"]);
+    $valorRecargoNocturno = htmlspecialchars($_POST["valorRecargoNocturno"]);
+    $valorAuxilioTransporte = htmlspecialchars($_POST["valorAuxilioTransporte"]);
+    $valorSalud = htmlspecialchars($_POST["valorSalud"]);
+    $valorPension = htmlspecialchars($_POST["valorPension"]);
+    $ajustePorcentual = htmlspecialchars($_POST["ajustePorcentual"]) ? $_POST['ajustePorcentual'] : null;
+  
+  
+    if (!empty($salariobasico) && !empty($valorhora) && !empty($valorHoraExtraDiurna) && !empty($valorHoraExtraNocturna) && !empty($valorHoraExtraDominical) && !empty($valorHoraExtraDominicalNocturna) && !empty($valorHoraDomingosFestivos) && !empty($valorRecargoNocturno) && !empty($valorAuxilioTransporte) && !empty($valorSalud) && !empty($valorPension)) {
+  
+      try {
+        $sql = "INSERT INTO configuracion (salario_basico, valor_hora, valor_hora_extra_diurna, valor_hora_extra_nocturna, valor_hora_extra_dominical, valor_hora_extra_dominical_nocturna, valor_hora_domingos_festivos, valor_recargo_nocturno, valor_auxilio_transporte, valor_salud, valor_pension, ajuste_porcentual) VALUES (:salariobasico, :valorhora, :valorHoraExtraDiurna,:valorHoraExtraNocturna, :valorHoraExtraDominical, :valorHoraExtraDominicalNocturna, :valorHoraDomingosFestivos, :valorRecargoNocturno, :valorAuxilioTransporte, :valorSalud, :valorPension, :ajustePorcentual)";
+  
+        $stmt = $objconexion->prepare($sql);
+        $stmt->bindParam(':salariobasico', $salariobasico);
+        $stmt->bindParam(':valorhora', $valorhora);
+        $stmt->bindParam(':valorHoraExtraDiurna', $valorHoraExtraDiurna);
+        $stmt->bindParam(':valorHoraExtraNocturna', $valorHoraExtraNocturna);
+        $stmt->bindParam(':valorHoraExtraDominical', $valorHoraExtraDominical);
+        $stmt->bindParam(':valorHoraExtraDominicalNocturna', $valorHoraExtraDominicalNocturna);
+        $stmt->bindParam(':valorHoraDomingosFestivos', $valorHoraDomingosFestivos);
+        $stmt->bindParam(':valorRecargoNocturno', $valorRecargoNocturno);
+        $stmt->bindParam(':valorAuxilioTransporte', $valorAuxilioTransporte);
+        $stmt->bindParam(':valorSalud', $valorSalud);
+        $stmt->bindParam(':valorPension', $valorPension);
+        $stmt->bindParam(':ajustePorcentual', $ajustePorcentual);
+  
+        if ($stmt->execute()) {
+          echo "<script>
+          alert('Configuración Agregada Correctamente');
+          window.location.href = 'AjustesNomina.php';
+      </script>";
+        } else {
+          echo "Error al agregar Configuración.";
+        }
+      
+      } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
       }
-    } catch (PDOException $e) {
-      echo "Error: " . $e->getMessage();
+    } else {
+      echo "<script>alert('por favor rellenar todos los campos!!!!!');</script>";
     }
-  } else {
-    echo "<script>alert('por favor rellenar todos los campos!!!!!');</script>";
+  }else {
+    //echo "No se recibieron datos del formulario.";
   }
-}else {
-  //echo "No se recibieron datos del formulario.";
+
 }
+
+
 
 
 
@@ -89,72 +138,72 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       <div class="row mb-1">
         <div class="col-md-6 mb-3 mb-md-0">
           <label for="salarioBasico" class="form-label">Salario Basico:</label>
-          <input type="text" name="salarioBasico" class="form-control" id="numeroEntero_1" oninput="validarNumeroEntero('numeroEntero_1')" required>
+          <input type="text" name="salarioBasico" class="form-control" value="<?php echo htmlspecialchars($salariobasico = isset($row_verificar['salario_basico']) ? $row_verificar['salario_basico'] : ""); ?>" id="numeroEntero_1" oninput="validarNumeroEntero('numeroEntero_1')" required>
           <p id="mensajeError_1"></p>
         </div>
         <div class="col-md-6">
           <label for="valorHora" class="form-label">Valor Hora:</label>
-          <input type="text" name="valorHora" class="form-control" id="numeroEntero_2" oninput="validarNumeroEntero('numeroEntero_2')" required>
+          <input type="text" name="valorHora" class="form-control" value="<?php echo htmlspecialchars($valor_hora = isset($row_verificar['valor_hora']) ? $row_verificar['valor_hora'] : ""); ?>" id="numeroEntero_2" oninput="validarNumeroEntero('numeroEntero_2')" required>
           <p id="mensajeError_2"></p>
         </div>
       </div>
       <div class="row mb-1">
         <div class="col-md-6 mb-3 mb-md-0">
           <label for="valorHoraExtraDiurna" class="form-label">Valor Hora Extra Diurna:</label>
-          <input type="text" name="valorHoraExtraDiurna" class="form-control" id="numeroEntero_3" oninput="validarNumeroEntero('numeroEntero_3')" required>
+          <input type="text" name="valorHoraExtraDiurna" class="form-control" value="<?php echo htmlspecialchars($valor_hora_extra_diurna = isset($row_verificar['valor_hora_extra_diurna']) ? $row_verificar['valor_hora_extra_diurna'] : ""); ?>" id="numeroEntero_3" oninput="validarNumeroEntero('numeroEntero_3')" required>
           <p id="mensajeError_3"></p>
         </div>
         <div class="col-md-6">
           <label for="valorHoraExtraNocturna" class="form-label">Valor Hora Extra Nocturna:</label>
-          <input type="text" name="valorHoraExtraNocturna" class="form-control" id="numeroEntero_4" oninput="validarNumeroEntero('numeroEntero_4')" required>
+          <input type="text" name="valorHoraExtraNocturna" class="form-control" value="<?php echo htmlspecialchars($valor_hora_extra_nocturna = isset($row_verificar['valor_hora_extra_nocturna']) ? $row_verificar['valor_hora_extra_nocturna'] : ""); ?>" id="numeroEntero_4" oninput="validarNumeroEntero('numeroEntero_4')" required>
           <p id="mensajeError_4"></p>
         </div>
       </div>
       <div class="row mb-1">
         <div class="col-md-6 mb-3 mb-md-0">
           <label for="valorHoraExtraDominical" class="form-label">Valor Hora Extra Dominical:</label>
-          <input type="text" name="valorHoraExtraDominical" class="form-control" id="numeroEntero_5" oninput="validarNumeroEntero('numeroEntero_5')" required>
+          <input type="text" name="valorHoraExtraDominical" class="form-control" value="<?php echo htmlspecialchars($valor_hora_extra_dominical = isset($row_verificar['valor_hora_extra_dominical']) ? $row_verificar['valor_hora_extra_dominical'] : ""); ?>" id="numeroEntero_5" oninput="validarNumeroEntero('numeroEntero_5')" required>
           <p id="mensajeError_5"></p>
         </div>
         <div class="col-md-6">
           <label for="valorHoraExtraDominicalNocturna" class="form-label">Valor Hora Extra Dominical Nocturna:</label>
-          <input type="text" name="valorHoraExtraDominicalNocturna" class="form-control" id="numeroEntero_6" oninput="validarNumeroEntero('numeroEntero_6')" required>
+          <input type="text" name="valorHoraExtraDominicalNocturna" class="form-control" value="<?php echo htmlspecialchars($valor_hora_extra_dominical_nocturna = isset($row_verificar['valor_hora_extra_dominical_nocturna']) ? $row_verificar['valor_hora_extra_dominical_nocturna'] : ""); ?>" id="numeroEntero_6" oninput="validarNumeroEntero('numeroEntero_6')" required>
           <p id="mensajeError_6"></p>
         </div>
       </div>
       <div class="row mb-1">
         <div class="col-md-6 mb-3 mb-md-0">
           <label for="valorHoraDomingosFestivos" class="form-label">Valor Hora Domingos y Festivos:</label>
-          <input type="text" name="valorHoraDomingosFestivos" class="form-control" id="numeroEntero_7" oninput="validarNumeroEntero('numeroEntero_7')" required>
+          <input type="text" name="valorHoraDomingosFestivos" class="form-control" value="<?php echo htmlspecialchars($valor_hora_domingos_festivos = isset($row_verificar['valor_hora_domingos_festivos']) ? $row_verificar['valor_hora_domingos_festivos'] : ""); ?>" id="numeroEntero_7" oninput="validarNumeroEntero('numeroEntero_7')" required>
           <p id="mensajeError_7"></p>
         </div>
         <div class="col-md-6">
           <label for="valorRecargoNocturno" class="form-label">Valor Recargo Nocturno:</label>
-          <input type="text" name="valorRecargoNocturno" class="form-control" id="numeroEntero_8" oninput="validarNumeroEntero('numeroEntero_8')" required>
+          <input type="text" name="valorRecargoNocturno" class="form-control" value="<?php echo htmlspecialchars($valor_recargo_nocturno = isset($row_verificar['valor_recargo_nocturno']) ? $row_verificar['valor_recargo_nocturno'] : ""); ?>" id="numeroEntero_8" oninput="validarNumeroEntero('numeroEntero_8')" required>
           <p id="mensajeError_8"></p>
         </div>
       </div>
       <div class="row mb-1">
         <div class="col-md-6 mb-3 mb-md-0">
           <label for="valorAuxilioTransporte" class="form-label">Valor Auxilio de transporte:</label>
-          <input type="text" name="valorAuxilioTransporte" class="form-control" id="numeroEntero_9" oninput="validarNumeroEntero('numeroEntero_9')" required>
+          <input type="text" name="valorAuxilioTransporte" class="form-control" value="<?php echo htmlspecialchars($valor_auxilio_transporte = isset($row_verificar['valor_auxilio_transporte']) ? $row_verificar['valor_auxilio_transporte'] : ""); ?>" id="numeroEntero_9" oninput="validarNumeroEntero('numeroEntero_9')" required>
           <p id="mensajeError_9"></p>
         </div>
         <div class="col-md-6">
           <label for="valorSalud" class="form-label">Valor Salud:</label>
-          <input type="text" name="valorSalud" class="form-control" id="numeroEntero_10" oninput="validarNumeroEntero('numeroEntero_10')" required>
+          <input type="text" name="valorSalud" class="form-control" value="<?php echo htmlspecialchars($valor_salud = isset($row_verificar['valor_salud']) ? $row_verificar['valor_salud'] : ""); ?>" id="numeroEntero_10" oninput="validarNumeroEntero('numeroEntero_10')" required>
           <p id="mensajeError_10"></p>
         </div>
       </div>
       <div class="row mb-1">
         <div class="col-md-6 mb-3 mb-md-0">
           <label for="valorPension" class="form-label">Valor Pensión:</label>
-          <input type="text" name="valorPension" class="form-control" id="numeroEntero_11" oninput="validarNumeroEntero('numeroEntero_11')" required>
+          <input type="text" name="valorPension" class="form-control" value="<?php echo htmlspecialchars($valor_pension = isset($row_verificar['valor_pension']) ? $row_verificar['valor_pension'] : ""); ?>" id="numeroEntero_11" oninput="validarNumeroEntero('numeroEntero_11')" required>
           <p id="mensajeError_11"></p>
         </div>
         <div class="col-md-6 mb-3 mb-md-0">
           <label for="salarioBasico" class="form-label">Ajuste porcentual:</label>
-          <input type="text" name="ajustePorcentual" class="form-control">
+          <input type="text" name="ajustePorcentual" class="form-control" value="<?php echo htmlspecialchars($ajuste_porcentual = isset($row_verificar['ajuste_porcentual']) ? $row_verificar['ajuste_porcentual'] : ""); ?>">
         </div>
       </div>
       <div class="row">
